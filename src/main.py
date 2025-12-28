48b3d445fa4ba8fd7bef990f44ddff7e8f107185
bzlcode <basilpeter2006@gmail.com>
2025-12-28 22:35:16 +0530

    Update main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
-#AAH3 web_ui groq
+#web-based chatbot
 from flask import Flask, request, jsonify, render_template
 from dotenv import load_dotenv
 import os
 from ai_engine import get_groq_response
 
 load_dotenv()
-app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, "templates")
+)
 
 @app.route("/")
 def index():
